3fe27acd6ef6a2b22378898ab4333a61bd5dcb77
Mounika-guru39 <gurumounika29@gmail.com>
2025-09-15 19:39:18 +0530

    Create UserContext.js

---
+++ FullStack/Frontend/src/context/UserContext.js
@@ -0,0 +1,53 @@
+// frontend/src/context/UserContext.js
+import React, { createContext, useContext, useReducer } from 'react';
+
+const UserContext = createContext();
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_USERS':
+      return { ...state, users: action.payload, loading: false };
+    case 'ADD_USER':
+      return { ...state, users: [action.payload, ...state.users] };
+    case 'UPDATE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.payload.id ? action.payload : user
+        )
+      };
+    case 'DELETE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.payload)
+      };
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
+    case 'SET_ERROR':
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
+
+export const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(userReducer, {
+    users: [],
+    loading: false,
+    error: null
+  });
+
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUsers = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUsers must be used within a UserProvider');
+  }
+  return context;
+};
